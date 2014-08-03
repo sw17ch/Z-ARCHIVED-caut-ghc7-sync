04ecc5cfca40b5a220784928b2a4a143cc6a7008
John Van Enk <vanenkj@gmail.com>
2014-08-03 11:17:02 -0700

    Add Sets.

--- Cauterize/Generators/Hs2010/Synchronous/HSFile.hs
+++ Cauterize/Generators/Hs2010/Synchronous/HSFile.hs
@@ -107,18 +107,32 @@ typeSizer (BuiltIn (TBuiltIn t) _ s) = staticSize (biReprText t) s
 typeSizer (Scalar (TScalar n _) _ s) = staticSize (T.pack n) s
 typeSizer (Const (TConst n _ _) _ s) = staticSize (T.pack n) s
 typeSizer (FixedArray (TFixedArray n _ _) _ _) =
-  sizerInstance n (parens (sNameToTypeNameDoc n <+> "vec")) "(V.sum . V.map typeSizer) vec"
+  sizerInstance n (parens (sNameToTypeNameDoc n <+> "vec")) " = (V.sum . V.map typeSizer) vec"
 typeSizer (BoundedArray (TBoundedArray n _ _) _ _ _) =
-  sizerInstance n (parens (sNameToTypeNameDoc n <+> "len" <+> "vec")) "typeSizer len + (V.sum . V.map typeSizer) vec"
+  sizerInstance n (parens (sNameToTypeNameDoc n <+> "len" <+> "vec")) " = typeSizer len + (V.sum . V.map typeSizer) vec"
 typeSizer (Struct (TStruct n (Fields fs)) _ _) =
-  let flabels = take (length fs) $ map (("f" `T.append`) . T.pack . show) ([0..] :: [Integer])
-  in sizerInstance n (parens (sNameToTypeNameDoc n <+> (text . T.unwords) flabels)) (text $ T.intercalate " + " (map ("typeSizer " `T.append`) flabels))
-typeSizer t = sizerInstance (typeName t) "(TheType f)" "?????????????????????????????"
+  let flabels = fieldArgs fs
+  in sizerInstance n (matchFields n flabels)
+                     (encloseSep " = " empty " + " (map (\x -> text $ "typeSizer " `T.append` x) flabels))
+typeSizer (Set (TSet n (Fields fs)) _ _ _) =
+  let flabels = fieldArgs fs
+  in sizerInstance n (withTypeMatchFields "t" n flabels)
+                     (encloseSep " = " empty " + " ("(cautSize . tagRepr) t" : map (\x -> text $ "maybe 0 typeSizer " `T.append` x) flabels))
+typeSizer t = sizerInstance (typeName t) "(TheType f)" "= ?????????????????????????????"
+
+withTypeMatchFields :: Doc -> String -> [T.Text] -> Doc
+withTypeMatchFields t n flabels = t <> "@" <> parens (sNameToTypeNameDoc n <+> (text . T.unwords) flabels)
+
+matchFields :: String -> [T.Text] -> Doc
+matchFields n flabels = parens $ sNameToTypeNameDoc n <+> (text . T.unwords) flabels
+
+fieldArgs :: [a] -> [T.Text]
+fieldArgs fs = take (length fs) $ map (("f" `T.append`) . T.pack . show) ([0..] :: [Integer])
 
 sizerInstance :: String -> Doc -> Doc -> Doc
 sizerInstance n pat rhs =
   vcat [ "instance CauterizeSize" <+> sNameToTypeNameDoc n <+> "where"
-       , indent 2 $ "cautSize" <+> pat <+> "=" <+> rhs
+       , indent 2 $ "cautSize" <+> pat <> rhs
        ]
 
 fixedSizeDoc :: FixedSize -> Doc
